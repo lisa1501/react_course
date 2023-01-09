72c79602ec210d9a6568e6361f10e5d40a05c1c3
halisa <halisasaipulaa@gmail.com>
2023-01-09 12:47:22 -0800

    first react practice

---
+++ index.js
@@ -0,0 +1,5 @@
+// ReactDOM.createRoot(document.getElementById('root')).render(<h1>Hello, world!</h1>);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello, world!</h1>); 
