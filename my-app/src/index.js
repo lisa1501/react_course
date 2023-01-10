e48c29385d840c855dc44f1f3028d7a028e75760
halisa <halisasaipulaa@gmail.com>
2023-01-10 09:30:45 -0800

    Build a React info site|Navbar and Styling

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Header  from './Header';
-import Footer from './Footer';
-import MainContent from './MainContent';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-function Page(){
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-    )
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Page />
+    <App />
 );
 reportWebVitals();
