42d697689be1ebf2031864fc65d03fdc305cad83
Lisa S <halisasaipulla@gmail.com>
2023-01-10 09:30:45 -0800

    Build a React info site|Navbar and Styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import Header  from './components/Header';
+import Footer from './Components/Footer';
+// import MainContent from './components/MainContent';
+import Main from './Components/Main';
+import Navbar from './Components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import Navbar from 'reactstrap/lib/Navbar';
+// import Nav from 'reactstrap/lib/Nav';
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default function App(){
+    return (
+        <div>
+            <Navbar />
+            <Main />
+            <Footer />
+        </div>
+    )
+};
