ed5180ed9270508bc9c4c7a1f36adf7cdead232e
halisa <halisasaipulaa@gmail.com>
2023-01-09 19:45:42 -0800

    Project1-React page-mark up

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,11 +5,24 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const page = (
+    <div>
+        <img src="./react-logo.png" width="40px" />
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walkes</li>
+            <li>Has well over 100k stars on Github</li>
+            <li>Is Maitained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>        
+        </ul>
+    </div>
+)
 root.render(
 //   <React.StrictMode>
 //     <App />
 //   </React.StrictMode>
-<h1>Hello world!</h1>
+    page
 );
 
 // If you want to start measuring performance in your app, pass a function
