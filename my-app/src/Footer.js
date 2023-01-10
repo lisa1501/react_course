708ee385fbff49bdeefc74752bd2a29ee94cd8d2
Lisa S <halisasaipulla@gmail.com>
2023-01-09 20:55:32 -0800

    Build a react info sit| Organize Components

---
+++ my-app/src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer(){
+    return (
+        <footer>
+            <small>© Saipulla development. all rights reserved.</small>
+        </footer>
+    )
+}
