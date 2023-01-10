d2c8ef3edec3359d9873c4fba791bbeada001e8c
Lisa S <halisasaipulla@gmail.com>
2023-01-10 09:54:21 -0800

    Build a React info site| Main Section

--- my-app/src/Components/Main.js
+++ my-app/src/Components/Main.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Main() {
-    return <h1>Main Components</h1>
+    return (
+        <main>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walkes</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is Maitained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>        
+            </ul>
+
+        </main>
+    )
 }
