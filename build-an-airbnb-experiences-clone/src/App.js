c50f9fe53ffb91f17e4e0b54599fb0ff71d92e7a
halisa <halisasaipulaa@gmail.com>
2023-01-10 13:28:04 -0800

    Build an Airbnb Experiences Clone|Project Setop: Hero

--- build-an-airbnb-experiences-clone/src/App.js
+++ build-an-airbnb-experiences-clone/src/App.js
@@ -1,15 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
 
 
 function App() {
-  return (
-    <div>
-        <Navbar />
-    </div>
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+        </div>
     
-  );
+    );
 }
 
 export default App;
