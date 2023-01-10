e4befdef4221a2034c43ac03fd351418a91abdc8
halisa <halisasaipulaa@gmail.com>
2023-01-09 20:55:32 -0800

    Build a react info sit| Organize Components

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,68 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Header  from './Header';
+import Footer from './Footer';
+import MainContent from './MainContent';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function Header(){
-    return (
-        <header>
-        <nav>
-            <img src="./react-logo.png" width="40px" />
-            <ul>
-                <li>Pricing</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
-    </header>
 
-    )
-}
-function Footer(){
-    return (
-        <footer>
-                <small>© Saipulla development. all rights reserved.</small>
-            </footer>
-    )
-}
-
-function MainContent(){
-    return (
-        <div>
-            <h1>Fun facts about React</h1>
-            <ol>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walkes</li>
-                <li>Has well over 100k stars on Github</li>
-                <li>Is Maitained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>        
-            </ol>
-
-        </div>
-
-    )
-}
 function Page(){
     return (
         <div>
             <Header />
             <MainContent />
-            
             <Footer />
         </div>
     )
 }
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
     <Page />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
