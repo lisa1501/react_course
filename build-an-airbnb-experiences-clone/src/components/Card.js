c4b5422bb4cfcf1784f9c2df44da2fdffc9e78d0
Lisa S <halisasaipulla@gmail.com>
2023-01-11 10:56:25 -0800

    Build an Airbnb Experiences Clone|Project Setop: Pass props to component

--- build-an-airbnb-experiences-clone/src/components/Card.js
+++ build-an-airbnb-experiences-clone/src/components/Card.js
@@ -2,21 +2,20 @@ import React from "react";
 import cardPhoto from "../images/card-photo.png"
 import starPhoto from "../images/star.png"
 import "./Card.css"
-function Card() {
+function Card(props) {
+    console.log(props)
     return (
         <div className="card">
-            <img src={cardPhoto} className="card--photo"/>
+            <img src={`../images/${props.img}`} className="card--photo"/>
             <div className="card--status">
                 <img src={starPhoto} className="card--star"/>
-                <span className="gray">5.0</span>
-                <span className="gray">(6) ·</span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount})·</span>
+                <span className="gray">{props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
 export default Card;
-
-// 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
