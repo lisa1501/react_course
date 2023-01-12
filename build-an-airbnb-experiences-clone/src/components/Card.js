7ad04dfae1122c52d287f8a783a748b4e5ab8c96
halisa <halisasaipulaa@gmail.com>
2023-01-11 20:47:03 -0800

    Build an Airbnb Experiences Clone|Project: SOLD OUT Badge

--- build-an-airbnb-experiences-clone/src/components/Card.js
+++ build-an-airbnb-experiences-clone/src/components/Card.js
@@ -3,15 +3,25 @@ import React from "react";
 // import starPhoto from "../images/star.png"
 import "./Card.css"
 function Card(props) {
-    console.log(props)
+    console.log(props.item)
+    let badgeText
+    if (props.iopenSpots === 0) {
+        badgeText = "SOLD OUT"
+    }else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
     return (
         <div className="card">
+            {
+                badgeText && 
+                <div className="card--badge">{badgeText}</div>
+            }           
             <img src={`../images/${props.img}`} className="card--photo"/>
             <div className="card--status">
                 <img src={"../images/star.png"} className="card--star"/>
                 <span>{props.rating}</span>
                 <span className="gray">({props.reviewCount})·</span>
-                <span className="gray">{props.country}</span>
+                <span className="gray">{props.location}</span>
             </div>
             <p>{props.title}</p>
             <p><span className="bold">From ${props.price}</span> / person</p>
