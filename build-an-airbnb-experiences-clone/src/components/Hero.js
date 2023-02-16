e7c8adb07f89debb8aa2cdb1538ca07985585d1d
Lisa S <halisasaipulla@gmail.com>
2023-02-15 21:28:48 -0800

    update airbnb online experiences content

--- build-an-airbnb-experiences-clone/src/components/Hero.js
+++ build-an-airbnb-experiences-clone/src/components/Hero.js
@@ -5,8 +5,7 @@ function Hero() {
         <section className="hero">
             <img src={"../images/photo-grid.png"} className="hero--photo"/>
             <h1 className="hero--header">Online Experiences</h1>
-            <p className="hero--text">Join unique interactive activities led by
-                one-of-a-kind hosts-all without leaving home.
+            <p className="hero--text">Plan a trip with help from local Hosts around the world
             </p>
         </section>
 
